e0dbfc13fbb197e79959fef824c84f2080ee8dfa
Mohammed <Moeldowy@iti.gov.eg>
2021-06-11 23:26:47 +0200

    refactoring files

--- index.php
+++ index.php
@@ -1,2 +1,22 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
 <?php
-echo "hi SWE Sohag";
+echo "<h1>hi SWE Sohag Q3</h1>";
+?>
+<ul>
+    <li><a href="Day1">Day1</a></li>
+    <li><a href="Day2">Day2</a></li>
+    <li><a href="Day3">Day3</a></li>
+    <li><a href="Day4">Day4</a></li>
+    <li><a href="Day5">Day5</a></li
+</ul>
+</body>
+</html>
